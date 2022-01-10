94274721430f3937f98baceed0ce18b1fa5219b6
CauaSDO <cauasilva@icloud.com>
2022-01-10 12:00:11 -0300

    Initial Commit

---
+++ CSO14-Swift-tuplas.playground/Contents.swift
@@ -0,0 +1,15 @@
+//Tuplas
+
+//let addres = "Av. Paulista, 1578, São Paulo - SP, 01310-200"
+
+
+let address: (type: String, name: String, number: Int, city: String, state: String, cep: String) = ("Av", "Paulista", 1578, "São Paulo", "SP", "01310-200")
+
+print(address.self)
+print(address.type)
+print(address.name)
+print(address.number)
+print(address.3)
+print(address.4)
+print(address.5)
+
